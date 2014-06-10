35507253bf860f64d156a637144e9d4ee83e0373
Harish Bhosle <bhosle91@gmail.com>
2014-06-11 00:03:24 +0530

    changes made in HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,16 @@
 
 public class HelloWorld{
 	
+	private String _sMessege = "Hello World!!";
+	
 	public static void main(String args[]) {
-		System.out.println("Hello World!!");
+		
+		String sMessege = getMessege();
+		System.out.println(sMessege);
 	}
+	
+	public String getMessege() {
+		return this._sMessege;
+	}
+	
 } 
